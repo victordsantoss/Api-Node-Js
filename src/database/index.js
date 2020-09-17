011d5df21103df851075eed4a3901b9a6c01a761
Victor Samuel <victor.samuelsantoss@gmail.com>
2020-09-17 13:01:09 -0300

    Creation and addition of folders, modules, packages and code.

---
+++ src/database/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://localhost/noderest", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+module.exports = mongoose;
